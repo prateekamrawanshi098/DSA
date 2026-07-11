0d41c1db2656bf896451687fc15a33fa7ce60f29
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-07-11 10:47:57 +0530

    LinkedList

---
+++ Day-7/02-Question1.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Question1{
+    public static void main(String[] args) {
+        List<Integer> numList = new LinkedList<>();
+        numList.add(10);
+        numList.add(20);
+        numList.add(30);
+        numList.add(40);
+        numList.add(50);
+        System.out.println(numList);
+    }
+}
---
+++ Day-7/03-Question2.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Question2{
+    public static void main(String[] args) {
+        List<String> studentList = new LinkedList<>();
+        studentList.add("Manoj");
+        studentList.add("Sandhya");
+        studentList.add("Ravi");
+        studentList.add("Tanya");
+        studentList.add("Kishan");
+        System.out.println(studentList);
+    }
+}
